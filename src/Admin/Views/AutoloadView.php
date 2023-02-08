c1740c617514657e235f6c761576cf3acd7b5866
Tiago Hillebrandt <tiago@tiagohillebrandt.com>
2023-02-08 02:35:11 -0500

    Outputs the stats for transients and autoload views.

--- src/Admin/Views/AutoloadView.php
+++ src/Admin/Views/AutoloadView.php
@@ -25,7 +25,36 @@ final class AutoloadView extends AbstractView {
 			<div class="content">
 				<p>WordPress autoload options are very similar to transients. The main difference is: transients are used to store temporary data, while options are used to store permanent data.</p>
 				<p>All the autoload options, as well as transients, are loaded automatically when WordPress loads itself. Thus, the number and size of these options can directly affect your site performance.</p>
+
+				<?php $this->output_stats(); ?>
 			</div>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Outputs the stats.
+	 *
+	 * @since 1.0.0
+	 */
+	public function output_stats() {
+
+		$stats = supv()->core()->autoload()->get_stats();
+		?>
+		<div class="supv-stats">
+			<ul>
+				<li>
+					<p class="supv-stats-title">Total</p>
+					<p class="supv-stats-data"><span><?php echo ! empty( $stats['count'] ) ? esc_html( $stats['count'] ) : '0'; ?></span></p>
+				</li>
+				<li>
+					<p class="supv-stats-title">Size</p>
+					<p class="supv-stats-data">
+						<span><?php echo ! empty( $stats['size'] ) && is_numeric( $stats['size'] ) ? esc_html( round( $stats['size'], 2 ) ) : '0.00'; ?></span>
+						<span class="supv-stats-footer">Mb</span>
+					</p>
+				</li>
+			</ul>
 		</div>
 		<?php
 	}
--- src/Admin/Views/TransientsView.php
+++ src/Admin/Views/TransientsView.php
@@ -25,7 +25,36 @@ final class TransientsView extends AbstractView {
 			<div class="content">
 				<p>WordPress transients are used to temporarily cache specific data. For example, developers often use them to improve their themes and plugins performance by caching database queries and script results.</p>
 				<p>However, some badly coded plugins and themes can store too much information on these transients, or can even create an excessively high number of transients, resulting in performance degradation.</p>
+
+				<?php $this->output_stats(); ?>
 			</div>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Outputs the stats.
+	 *
+	 * @since 1.0.0
+	 */
+	public function output_stats() {
+
+		$stats = supv()->core()->transients()->get_stats();
+		?>
+		<div class="supv-stats">
+			<ul>
+				<li>
+					<p class="supv-stats-title">Total</p>
+					<p class="supv-stats-data"><span><?php echo ! empty( $stats['count'] ) ? esc_html( $stats['count'] ) : '0'; ?></span></p>
+				</li>
+				<li>
+					<p class="supv-stats-title">Size</p>
+					<p class="supv-stats-data">
+						<span><?php echo ! empty( $stats['size'] ) && is_numeric( $stats['size'] ) ? esc_html( round( $stats['size'], 2 ) ) : '0.00'; ?></span>
+						<span class="supv-stats-footer">Mb</span>
+					</p>
+				</li>
+			</ul>
 		</div>
 		<?php
 	}
