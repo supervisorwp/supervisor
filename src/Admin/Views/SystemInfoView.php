ecd1b2d812c8feaed8753a9fbdcd4c125d65cae3
Tiago Hillebrandt <tiago@tiagohillebrandt.com>
2023-02-20 23:00:49 -0500

    Fix typo.

--- src/Admin/Views/SystemInfoView.php
+++ src/Admin/Views/SystemInfoView.php
@@ -91,7 +91,7 @@ final class SystemInfoView extends AbstractView {
 				break;
 
 			case 'outdated':
-				$title = esc_html__( 'Your software is outdated, but it still compatible with WordPress', 'supervisor' );
+				$title = esc_html__( 'Your software is outdated, but it is still compatible with WordPress', 'supervisor' );
 				break;
 
 			case 'obsolete':
