df7a636e97220191f45fdbc01e4331d7c471c065
Tiago Hillebrandt <tiago@tiagohillebrandt.com>
2023-02-23 00:30:26 -0500

    Adds action to clean up the transients after WordPress updates.

--- src/Utils/Upgrade.php
+++ src/Utils/Upgrade.php
@@ -25,7 +25,33 @@ final class Upgrade {
 	 */
 	public function __construct() {
 
+		$this->hooks();
 		$this->maybe_upgrade_db();
+	}
+
+	/**
+	 * WordPress actions and filters.
+	 *
+	 * @since {VERSION}
+	 */
+	public function hooks() {
+
+		add_action( 'upgrader_process_complete', 'upgrade_completed', 10, 2 );
+	}
+
+	/**
+	 * Cleans up the transients after WordPress updates.
+	 *
+	 * @since {VERSION}
+	 *
+	 * @param WP_Upgrader $upgrader The WP_Upgrader instance.
+	 * @param array       $options  The update data.
+	 */
+	public function upgrade_completed( $upgrader, $options ) {
+
+		if ( $options['action'] === 'update' && $options['type'] === 'core' ) {
+			Install::cleanup_plugin_options( true );
+		}
 	}
 
 	/**
